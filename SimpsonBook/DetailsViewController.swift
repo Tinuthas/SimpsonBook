c0de88abc2dea190973d3ba8baa16a1f5f58f75c
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2019-12-08 22:07:52 -0300

    create new controller to details e table view

---
+++ SimpsonBook/DetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsViewController.swift
+//  SimpsonBook
+//
+//  Created by Marcus Vinicius Galdino Medeiros on 08/12/19.
+//  Copyright © 2019 Marcus Vinicius Galdino Medeiros. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Marcus Vinicius Galdino Medeiros on 08/12/19.
+//  Copyright © 2019 Marcus Vinicius Galdino Medeiros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
